e4bfc885f703ee1d47ab3dfa7c317c9f8b21ca8e
FedericoRaschiatore <81446699+FedericoRaschiatore@users.noreply.github.com>
2021-07-01 12:59:05 +0200

    Aggiunto pagamento tickets

--- myBus/src/app/pages/tickets/tickets.page.ts
+++ myBus/src/app/pages/tickets/tickets.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.page.html',
   styleUrls: ['./tickets.page.scss'],
 })
-export class TicketsPage implements OnInit {
+export class TicketsPage {
+  paymentAmount: string = '1.20';
+  currency: string = 'USD';
+  currencyIcon: string = '$';
+  constructor() {
+    let _this = this;
+    setTimeout(() => {
+      // Render the PayPal button into #paypal-button-container
+      <any>window['paypal'].Buttons({
 
-  constructor() { }
+        // Set up the transaction
+        createOrder: function (data, actions) {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: _this.paymentAmount
+              }
+            }]
+          });
+        },
 
-  ngOnInit() {
+        // Finalize the transaction
+        onApprove: function (data, actions) {
+          return actions.order.capture()
+            .then(function (details) {
+              // Show a success message to the buyer
+              alert('Transaction completed by ' + details.payer.name.given_name + '!');
+            })
+            .catch(err => {
+              console.log(err);
+            })
+        }
+      }).render('#paypal-button-container');
+    }, 500)
   }
-
 }

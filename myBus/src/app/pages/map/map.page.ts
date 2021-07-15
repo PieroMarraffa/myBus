e3d0f63235f10831a51867b2078e0ade296a2069
FedericoRaschiatore <81446699+FedericoRaschiatore@users.noreply.github.com>
2021-07-15 19:47:32 +0200

    map aggiornato

--- myBus/src/app/pages/map/map.page.ts
+++ myBus/src/app/pages/map/map.page.ts
@@ -2,6 +2,10 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { StopListService } from "../../services/stop-list.service";
 import {NavController, NavParams} from "@ionic/angular";
+import { BusListService } from '../../services/bus-list.service';
+import {Bus} from "../../models/bus.model";
+import {BusListPage} from "../bus-list/bus-list.page";
+import {RouterLink} from "@angular/router";
 
 
 declare var google;
@@ -15,7 +19,7 @@ export class MapPage implements OnInit {
   @ViewChild('map') mapElement;
 
   stopList: any;
-
+  busList:any;
   map: any;
   mapOptions: any;
   mapCenter = { lat: null, lng: null };
@@ -24,12 +28,15 @@ export class MapPage implements OnInit {
 
   constructor(private geolocation: Geolocation,
               private stopListServices: StopListService,
+              private busListServices:BusListService,
               public navCtrl: NavController) {
 
 
   }
 
-  ngOnInit() {  this.stopListServices.stop$.subscribe(stop => {
+  ngOnInit() {
+    this.busListServices.buses$.subscribe(bus => {this.busList = bus});
+    this.stopListServices.stop$.subscribe(stop => {
     this.stopList = stop;
     this.loadMarker();
     this.loadMap();
@@ -41,10 +48,11 @@ export class MapPage implements OnInit {
     for(let i=0; i<this.stopList.length; i++){
       let stop: any;
       stop=this.stopList[i];
+      let id=stop.id;
       let title=stop.nome
       let latitude= stop.latitudine;
       let longitude= stop.longitudine;
-      let marker=[title,latitude,longitude];
+      let marker=[title,latitude,longitude,id];
       this.markers.push(marker);
     }
   }
@@ -52,13 +60,12 @@ export class MapPage implements OnInit {
   addMarker(){
     for(let marker of this.markers){
       let position= new google.maps.LatLng(marker[1],marker[2]);
-
       let mapMarker= new google.maps.Marker({
         map: this.map,
         position: position,
         title: marker[0],
+        id: marker[3],
       })
-      console.log(mapMarker);
       this.addInfoWindow(mapMarker);
     }
   }
@@ -81,25 +88,49 @@ export class MapPage implements OnInit {
     });
 
   }
-  addInfoWindow(marker){
-    let contentString = ( '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading"><b>' +  marker.title + ' </b></h1>' + "</div>" +
-      "</div>")
-    const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-    });
-    console.log(marker[0]);
-    google.maps.event.addListener(marker, "click", (function(marker) {
-      return function() {
-        var content = marker.getTitle();
-        infowindow.setContent(contentString);
-        infowindow.open(this.map, marker);
-      }
-    })(marker));
-
+  goToBus(){
+   this.navCtrl.navigateRoot("");
   }
+  addInfoWindow(marker){
+    let fermata = this.getStoppino(marker.id);
+    let box: string;
+    console.log(fermata,marker.id);
+    if(fermata!=false) {
+      let  cont=0;
+      for(let i of fermata){
+      let contentString = ('<ion-button  href="bus-list/Bus.Id">' +
+        '<h1 id="firstHeading" class="firstHeading">' + fermata[cont].nome + '</h1></ion-button>')
+      box = box + contentString;
+      cont ++;}
+
+      const infowindow = new google.maps.InfoWindow({
+
+        content: box,
+      });
+      google.maps.event.addListener(marker, "click", (function (marker) {
+        return function () {
+          infowindow.setContent(box);
+          infowindow.open(this.map, marker);
+        }
+      })(marker));
+    }
+  }
+  getStoppino(Stop){
+    let bus_per_fermata= [];
+    for( let bus of this.busList){
+      var key = (Object.keys(bus.percorso) as Array<string>);
+      for(let k of key){
+        if (Stop == k) {
+          bus_per_fermata.push(bus);
+        }
+        }
+      }
+    if(bus_per_fermata.length>0){
+       return bus_per_fermata;}
+    else{return false;}
+  }
+
+
 
 }
 

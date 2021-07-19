d96c1335ea01959833a14f1902c28c5cb8fbcb22
Gabriele Giusti <64482639+GabrieleG99@users.noreply.github.com>
2021-07-19 16:47:49 +0200

    small changes

--- myBus/src/app/pages/account/account.page.ts
+++ myBus/src/app/pages/account/account.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from "@ionic/angular";
+import {AlertController, NavController} from "@ionic/angular";
 import {User} from "../../models/user.model";
 import {AngularFirestore} from "@angular/fire/firestore";
 import {UsersService} from "../../services/users.service";
@@ -14,9 +14,12 @@ export class AccountPage implements OnInit {
 
   user: any;
 
+  profileImg: any = '😜';
+
   constructor(private navController: NavController,
               private afs: AngularFirestore,
-              private userService: UsersService) { }
+              private userService: UsersService,
+              private alertController: AlertController) { }
 
   ngOnInit() {
     this.userService.user$.subscribe(user => {
@@ -36,4 +39,106 @@ export class AccountPage implements OnInit {
     this.navController.navigateRoot('tickets');
   }
 
+  async presentAlertRadio() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Radio',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: '😀',
+          value: 'value1',
+          handler: () => {
+            for (let radioButton of alert.inputs){
+              if (radioButton.checked == true) radioButton.checked = false;
+            }
+            alert.inputs[0].checked = true;
+          },
+          //checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: '😇',
+          value: 'value2',
+          handler: () => {
+            for (let radioButton of alert.inputs){
+              if (radioButton.checked == true) radioButton.checked = false;
+            }
+            alert.inputs[1].checked = true;          }
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: '😊',
+          value: 'value3',
+          handler: () => {
+            for (let radioButton of alert.inputs){
+              if (radioButton.checked == true) radioButton.checked = false;
+            }
+            alert.inputs[2].checked = true;          }
+        },
+        {
+          name: 'radio4',
+          type: 'radio',
+          label: '😡',
+          value: 'value4',
+          handler: () => {
+            for (let radioButton of alert.inputs){
+              if (radioButton.checked == true) radioButton.checked = false;
+            }
+            alert.inputs[3].checked = true;          }
+        },
+        {
+          name: 'radio5',
+          type: 'radio',
+          label: '😰',
+          value: 'value5',
+          handler: () => {
+            for (let radioButton of alert.inputs){
+              if (radioButton.checked == true) radioButton.checked = false;
+            }
+            alert.inputs[4].checked = true;          }
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: '😜',
+          value: 'value6',
+          handler: () => {
+            for (let radioButton of alert.inputs){
+              if (radioButton.checked == true) radioButton.checked = false;
+            }
+            alert.inputs[5].checked = true;
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            this.changeProfileImg(alert.inputs);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async changeProfileImg(radioList){
+    for (let radioButton of radioList){
+      if (radioButton.checked){
+        this.profileImg = radioButton.label;
+      }
+    }
+  }
 }
